568fae39a66d6237d5b5f42c96557ccc90b2793f
connstance <connstance@gmail.com>
2012-06-13 20:04:20 +0400

    Add support saving path. Change hierarchy of working directory.

--- resizer.sh
+++ resizer.sh
@@ -2,43 +2,39 @@
 
 conv='/usr/local/bin/convert'
 zipping='/usr/local/bin/zip -q -0 -X -r'
+tmp_path='tmp'
+source_img=$1
+arch_path=$2
 
+if [ -z "$1" ]; then
+    printf '\n'"No objects for treatment. Exiting..."'\n''\n'
+    exit 1
+fi
 
 unsucc_test()
 {
 	if [ "$?" != "0" ]; then
-		rm -rf "$dir_name"
+		rm -rf "$work_path"
 		printf '\n'"Something wrong. Exiting..."'\n''\n'
     	exit 1
 	fi
 }
 
-if [ -z "$1" ]
-    then
-        printf '\n'"No objects for treatment. Exiting..."'\n''\n'
-        exit 1
-fi
-
-dir_name=`echo "$1" | sed -e 's/^.*\///' | sed -e 's/\.[^.]*$//'`
-png_path="$dir_name"/png
+dir_name=`echo "$source_img" | sed -e 's/^.*\///' | sed -e 's/\.[^.]*$//'`
+work_path="$tmp_path"/"$dir_name"
+png_path="$work_path"/png
 mkdir -p "$png_path"
-mkdir "$dir_name"/ico
+mkdir "$work_path"/ico
 
-for size in 256 128 108 92 72 64 60 48 40 32 24 16
-	do
-		$conv "$1" -resize "$size"x"$size"! -filter Lanczos "$png_path"/$size.png
-		unsucc_test
-	done
+for size in 256 128 108 92 72 64 60 48 40 32 24 16; do
+	$conv "$source_img" -resize "$size"x"$size"! -filter Lanczos "$png_path"/$size.png
+	unsucc_test
+done
 
+$conv "$png_path"/*.png "$work_path"/ico/"$dir_name".ico
 
-$conv "$png_path"/*.png "$dir_name"/ico/"$dir_name".ico
-#unsucc_test
+cp "$source_img" "$png_path"/512.png
 
-cp "$1" "$png_path"/512.png
+$zipping "$arch_path"/"$dir_name".zip ./"$work_path"/
 
-$zipping "$dir_name".zip ./"$dir_name"/
-#unsucc_test
-
-rm -rf "$dir_name"
-
-# rm $1
+rm -rf "$work_path"
